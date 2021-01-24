66742062cabb48f2369004bbb70253e3e0da99c7
repl.it user <replituser@example.com>
2021-01-24 13:14:51 +0000

    added showCategories

--- main.py
+++ main.py
@@ -3,22 +3,26 @@ import os
 import requests
 
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print("Logged in as {0.user}".format(client))
+client = discord.Client()
 
-    async def on_message(self, message):
-        if message.author == client.user:
-            return
+@client.event
+async def on_ready():
+    print("Logged in as {0.user}".format(client))
 
-        if message.content == "$Hello":
-            await message.channel.send("Hello!")
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
 
-        if message.content == "$random":
-            data = requests.get("https://api.chucknorris.io/jokes/random")
-            data = data.json()
-            await message.channel.send(data["value"])
+    if message.content == "$random":
+        data = requests.get("https://api.chucknorris.io/jokes/random")
+        data = data.json()
+        await message.channel.send(data["value"])
 
+    if message.content == "$showCategories":
+        data = requests.get("https://api.chucknorris.io/jokes/categories")
+        data = data.json()
+        data = '\n'.join(data)
+        await message.channel.send(data)
 
-client = MyClient()
 client.run(os.getenv("TOKEN"))
